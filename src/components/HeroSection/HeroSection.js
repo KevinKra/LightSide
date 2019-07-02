33675afd8cd86a20ab17f3b7586f589ff1b3c95a
Kevin Krato <kratok08@gmail.com>
2019-07-02 09:12:20 -0600

    Call dynamic async functions on click

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -9,9 +9,13 @@ export default function HeroSection(props) {
         <h1>LIGHTSIDE</h1>
         <nav>
           <Link to={"content-section"} smooth={true} offset={0} duration={500}>
-            <button onClick={props.displayPeople}>PEOPLE</button>
-            <button onClick={props.displayPlanets}>PLANETS</button>
-            <button onClick={props.displayVehicles}>VEHICLES</button>
+            <button onClick={() => props.displayData("people")}>PEOPLE</button>
+            <button onClick={() => props.displayData("planets")}>
+              PLANETS
+            </button>
+            <button onClick={() => props.displayData("vehicles")}>
+              VEHICLES
+            </button>
           </Link>
         </nav>
       </div>
