2fc1fb78613511b114e67b17f8b4e4e4b9308ea8
Kevin Krato <kratok08@gmail.com>
2019-07-03 15:40:51 -0600

    Detach background image and add gradient screen feature

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -4,14 +4,15 @@ import "./HeroSection.scss";
 
 export default function HeroSection(props) {
   return (
-    <section className="hero-section background-image">
+    <section className="hero-section">
+      <div className="background-image" />
+      <div className="background-gradient" />
       <div className="hero-content">
         <header className="primary-header">
           <h1>LIGHTSIDE</h1>
           <p>The go-to resource for all things STARWARS</p>
         </header>
         <nav>
-          {/* <Link to={"content-section"} smooth={true} offset={0} duration={500}> */}
           <div className="primary-btns">
             <Link
               to={"content-section"}
@@ -50,27 +51,6 @@ export default function HeroSection(props) {
           >
             FAVORITES <span>[{props.totalFavorites}]</span>
           </Link>
-          {/* </Link> */}
-          {/* <Link to={"content-section"} smooth={true} offset={0} duration={500}>
-            <div className="primary-btns">
-              <button onClick={() => props.displayData("people")}>
-                PEOPLE
-              </button>
-              <button onClick={() => props.displayData("planets")}>
-                PLANETS
-              </button>
-              <button onClick={() => props.displayData("vehicles")}>
-                VEHICLES
-              </button>
-            </div>
-            <button
-              className={`favorites-btn ${!props.totalFavorites && "hidden"}`}
-              disabled={!props.totalFavorites && "true"}
-              onClick={props.displayFavorites}
-            >
-              FAVORITES <span>[{props.totalFavorites}]</span>
-            </button>
-          </Link> */}
         </nav>
       </div>
     </section>
