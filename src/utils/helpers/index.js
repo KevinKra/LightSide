d4b7cdd21fcca07675b1f394d043a3a5b27598aa
Kevin Krato <kratok08@gmail.com>
2019-07-04 12:05:59 -0600

    Add function that accepts ambigious number of args and copies arg based on element passed in

--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -8,3 +8,16 @@ export const fetchData = async data => {
   });
   return output;
 };
+
+export function updateAssociatedArray(element, ...args) {
+  let output = [];
+  for (let i = 1; i < arguments.length; i++) {
+    if (arguments[i].includes(element)) {
+      let copy = [...arguments[i]];
+      const index = copy.indexOf(element);
+      copy[index].favorited = false;
+      output = copy;
+    }
+  }
+  return output;
+}
