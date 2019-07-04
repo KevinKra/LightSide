ced15703eae3f14b0fe2b106328c1a767d1dcec2
Kevin Krato <kratok08@gmail.com>
2019-07-04 12:52:55 -0600

    Convert to functional component and improve JSX semantics

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,104 +1,94 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Card.scss";
 
-class Card extends Component {
-  render() {
-    const {
-      name,
-      gender,
-      hair_color,
-      eye_color,
-      birth_year,
-      mass,
-      height,
-      climate,
-      terrain,
-      population,
-      rotation_period,
-      orbital_period,
-      diameter,
-      model,
-      vehicle_class,
-      manufacturer,
-      cost_in_credits,
-      max_atmosphering_speed,
-      crew,
-      passengers,
-      cargo_capacity
-    } = this.props.element;
+export default function Card(props) {
+  const {
+    name,
+    gender,
+    hair_color,
+    eye_color,
+    birth_year,
+    mass,
+    height,
+    climate,
+    terrain,
+    population,
+    rotation_period,
+    orbital_period,
+    diameter,
+    model,
+    vehicle_class,
+    manufacturer,
+    cost_in_credits,
+    max_atmosphering_speed,
+    crew,
+    passengers,
+    cargo_capacity
+  } = props.element;
 
-    if (this.props.format === "person") {
-      return (
-        <article className="Card">
-          <section className="card-primary">
-            <h1>{name}</h1>
-            <p>gender: {gender}</p>
-            <p>hair: {hair_color}</p>
-            <p>eye color: {eye_color}</p>
-            <p>birth year: {birth_year}</p>
-            <p>mass: {mass}</p>
-            <p>height: {height}</p>
-          </section>
-          <div className="card-bg person-bg" />
-          {!this.props.element.favorited ? (
-            <button onClick={() => this.props.addToFavorites(name)}>✓</button>
-          ) : (
-            <button onClick={() => this.props.removeFromFavorites(name)}>
-              Remove
-            </button>
-          )}
-        </article>
-      );
-    }
-    if (this.props.format === "planet") {
-      return (
-        <article className="Card">
-          <section className="card-primary">
-            <h1>{name}</h1>
-            <p>climate: {climate}</p>
-            <p>terrain: {terrain}</p>
-            <p>population: {population}</p>
-            <p>day length: {rotation_period} hours</p>
-            <p>year length: {orbital_period} days</p>
-            <p>diameter: {diameter}</p>
-          </section>
-          <div className="card-bg planet-bg" />
-          {!this.props.element.favorited ? (
-            <button onClick={() => this.props.addToFavorites(name)}>✓</button>
-          ) : (
-            <button onClick={() => this.props.removeFromFavorites(name)}>
-              Remove
-            </button>
-          )}
-        </article>
-      );
-    }
-    if (this.props.format === "vehicle") {
-      return (
-        <article className="Card">
-          <section className="card-primary">
-            <h1>{name}</h1>
-            <p>model: {model}</p>
-            <p>class: {vehicle_class}</p>
-            <p>manufacturer: {manufacturer}</p>
-            <p>cost: {cost_in_credits}</p>
-            <p>maximum speed: {max_atmosphering_speed}</p>
-            <p>crew capacity: {crew}</p>
-            <p>passenger capacity: {passengers}</p>
-            <p>cargo capacity: {cargo_capacity}</p>
-          </section>
-          <div className="card-bg vehicle-bg" />
-          {!this.props.element.favorited ? (
-            <button onClick={() => this.props.addToFavorites(name)}>✓</button>
-          ) : (
-            <button onClick={() => this.props.removeFromFavorites(name)}>
-              Remove
-            </button>
-          )}
-        </article>
-      );
-    }
+  if (props.format === "person") {
+    return (
+      <article className="Card">
+        <section className="card-primary">
+          <h1>{name}</h1>
+          <p>gender: {gender}</p>
+          <p>hair: {hair_color}</p>
+          <p>eye color: {eye_color}</p>
+          <p>birth year: {birth_year}</p>
+          <p>mass: {mass}</p>
+          <p>height: {height}</p>
+        </section>
+        <div className="card-bg person-bg" />
+        {!props.element.favorited ? (
+          <button onClick={() => props.addToFavorites(name)}>Favorite</button>
+        ) : (
+          <button onClick={() => props.removeFromFavorites(name)}>✓</button>
+        )}
+      </article>
+    );
+  }
+  if (props.format === "planet") {
+    return (
+      <article className="Card">
+        <section className="card-primary">
+          <h1>{name}</h1>
+          <p>climate: {climate}</p>
+          <p>terrain: {terrain}</p>
+          <p>population: {population}</p>
+          <p>day length: {rotation_period} hours</p>
+          <p>year length: {orbital_period} days</p>
+          <p>diameter: {diameter}</p>
+        </section>
+        <div className="card-bg planet-bg" />
+        {!props.element.favorited ? (
+          <button onClick={() => props.addToFavorites(name)}>Favorite</button>
+        ) : (
+          <button onClick={() => props.removeFromFavorites(name)}>✓</button>
+        )}
+      </article>
+    );
+  }
+  if (props.format === "vehicle") {
+    return (
+      <article className="Card">
+        <p className="card-primary">
+          <h1>{name}</h1>
+          <p>model: {model}</p>
+          <p>class: {vehicle_class}</p>
+          <p>manufacturer: {manufacturer}</p>
+          <p>cost: {cost_in_credits}</p>
+          <p>maximum speed: {max_atmosphering_speed}</p>
+          <p>crew capacity: {crew}</p>
+          <p>passenger capacity: {passengers}</p>
+          <p>cargo capacity: {cargo_capacity}</p>
+        </p>
+        <div className="card-bg vehicle-bg" />
+        {!props.element.favorited ? (
+          <button onClick={() => props.addToFavorites(name)}>Favorite</button>
+        ) : (
+          <button onClick={() => props.removeFromFavorites(name)}>✓</button>
+        )}
+      </article>
+    );
   }
 }
-
-export default Card;
