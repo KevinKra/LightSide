fb852205e6d15f80686d6a52d6b6f30495f7e2db
Kevin Krato <kratok08@gmail.com>
2019-07-05 16:44:21 -0600

    Create ContentSection tests file

---
+++ src/components/ContentSection/ContentSection.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ContentSection from "./ContentSection";
+
+describe("<ContentSection />", () => {
+  it.skip("should render without errors", () => {});
+});
