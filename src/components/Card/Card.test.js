3284ab107797877124baf9d2047490d14b6dac35
Kevin Krato <kratok08@gmail.com>
2019-07-07 17:53:55 -0600

    Remake snapshot tests

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,48 +1,54 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Card from "./Card";
-import * as mockData from "../../utils/mockData";
+import toJSON from "enzyme-to-json";
+import Card from "../Card/Card";
+
+const mockPerson = {
+  name: "Joe",
+  gender: "Male",
+  hair_color: "Green",
+  eye_color: "Blue",
+  birth_year: 1995,
+  mass: 120,
+  height: 72
+};
+
+export const mockVehicle = {
+  name: "Speeder",
+  model: "Super",
+  vehicle_class: "X12",
+  manufacturer: "Toyota",
+  const_in_credits: 15000,
+  max_atmosphere_speed: 50,
+  crew: 15,
+  passengers: 30,
+  cargo_capacity: 5
+};
+
+const mockPlanet = {
+  name: "Naboo",
+  climate: "Moderate",
+  terrain: "Grasslands",
+  population: 5000000,
+  rotation_period: 24,
+  orbital_period: 375,
+  diameter: 15
+};
 
 describe("<Card />", () => {
-  let mockAddToFavorites, mockRemoveFromFavorites;
-  beforeEach(() => {
-    mockAddToFavorites = jest.fn();
-    mockRemoveFromFavorites = jest.fn();
+  it("should render in the person format", () => {
+    const wrapper = shallow(<Card format="person" element={mockPerson} />);
+    // console.log(wrapper.debug());
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
-  it("should match the snapshot when 'person' is passed in as the format", () => {
-    const wrapper = shallow(
-      <Card
-        format="person"
-        element={mockData.mockPersonData}
-        addToFavorites={mockAddToFavorites}
-        removeFromFavorites={mockRemoveFromFavorites}
-      />
-    );
-    expect(wrapper).toMatchSnapshot();
+  it("should render in the vehicle format", () => {
+    const wrapper = shallow(<Card format="vehicle" element={mockVehicle} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
-  it("should match the snapshot when 'planet' is passed in as the format", () => {
-    const wrapper = shallow(
-      <Card
-        format="planet"
-        element={mockData.mockPlanetData}
-        addToFavorites={mockAddToFavorites}
-        removeFromFavorites={mockRemoveFromFavorites}
-      />
-    );
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("should match the snapshot when 'vehicle' is passed in as the format", () => {
-    const wrapper = shallow(
-      <Card
-        format="vehicle"
-        element={mockData.mockVehicleData}
-        addToFavorites={mockAddToFavorites}
-        removeFromFavorites={mockRemoveFromFavorites}
-      />
-    );
-    expect(wrapper).toMatchSnapshot();
+  it("should render in the planet format", () => {
+    const wrapper = shallow(<Card format="planet" element={mockPlanet} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
