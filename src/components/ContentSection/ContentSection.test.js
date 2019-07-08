f3803a5ba50cbc2e350eadbccf8db073708cd95c
Kevin Krato <kratok08@gmail.com>
2019-07-08 07:16:56 -0600

    Snapshot test component

--- src/components/ContentSection/ContentSection.test.js
+++ src/components/ContentSection/ContentSection.test.js
@@ -1,7 +1,39 @@
 import React from "react";
 import { shallow } from "enzyme";
+import toJSON from "enzyme-to-json";
 import ContentSection from "./ContentSection";
 
+const mockContentArray = [
+  {
+    name: "Joe",
+    gender: "Male",
+    hair_color: "Green",
+    eye_color: "Blue",
+    birth_year: 1995,
+    mass: 120,
+    height: 72,
+    id: 1
+  },
+  {
+    name: "Tony",
+    gender: "Unknown",
+    hair_color: "Orange",
+    eye_color: "Yellow",
+    birth_year: 1092,
+    mass: 10,
+    height: 150,
+    id: 2
+  }
+];
+
 describe("<ContentSection />", () => {
-  it.skip("should render without errors", () => {});
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <ContentSection content={mockContentArray} theme="person" />
+    );
+  });
+  it("should render without errors", () => {
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
 });
