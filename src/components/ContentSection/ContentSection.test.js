49f1d055f0bc62eb36b68b079786bb2bf62f7b48
Kevin Krato <kratok08@gmail.com>
2019-07-08 17:05:01 -0600

    Fully test snapshots

--- src/components/ContentSection/ContentSection.test.js
+++ src/components/ContentSection/ContentSection.test.js
@@ -3,7 +3,7 @@ import { shallow } from "enzyme";
 import toJSON from "enzyme-to-json";
 import ContentSection from "./ContentSection";
 
-const mockContentArray = [
+const mockContentArrayPeople = [
   {
     name: "Joe",
     gender: "Male",
@@ -26,14 +26,97 @@ const mockContentArray = [
   }
 ];
 
+const mockContentArrayPlanets = [
+  {
+    name: "Naboo",
+    climate: "Moderate",
+    terrain: "Grasslands",
+    population: 5000000,
+    rotation_period: 24,
+    orbital_period: 375,
+    diameter: 15,
+    favorited: false,
+    id: 1
+  },
+  {
+    name: "Tatooine",
+    climate: "Dry",
+    terrain: "Desert",
+    population: 50000,
+    rotation_period: 34,
+    orbital_period: 475,
+    diameter: 9,
+    favorited: false,
+    id: 2
+  }
+];
+
+const mockContentArrayVehicles = [
+  {
+    name: "Speeder",
+    model: "Super",
+    vehicle_class: "X12",
+    manufacturer: "Toyota",
+    const_in_credits: 15000,
+    max_atmosphere_speed: 50,
+    crew: 15,
+    passengers: 30,
+    cargo_capacity: 5,
+    favorited: false,
+    id: 1
+  },
+  {
+    name: "Slowboy",
+    model: "Sloweest",
+    vehicle_class: "z12",
+    manufacturer: "Yamaha",
+    const_in_credits: 150000,
+    max_atmosphere_speed: 1,
+    crew: 1,
+    passengers: 1,
+    cargo_capacity: 0,
+    favorited: false,
+    id: 2
+  }
+];
 describe("<ContentSection />", () => {
-  let wrapper;
+  let personTheme, planetTheme, vehicleTheme;
+  const mockAddToFavorites = jest.fn();
+  const mockRemoveFromFavorites = jest.fn();
+  const mockLogError = jest.fn();
   beforeEach(() => {
-    wrapper = shallow(
-      <ContentSection content={mockContentArray} theme="person" />
+    personTheme = shallow(
+      <ContentSection
+        content={mockContentArrayPeople}
+        theme="person"
+        addToFavorites={mockAddToFavorites}
+        removeFromFavorites={mockRemoveFromFavorites}
+        logError={mockLogError}
+      />
+    );
+    planetTheme = shallow(
+      <ContentSection
+        content={mockContentArrayPlanets}
+        theme="planet"
+        addToFavorites={mockAddToFavorites}
+        removeFromFavorites={mockRemoveFromFavorites}
+        logError={mockLogError}
+      />
+    );
+    vehicleTheme = shallow(
+      <ContentSection
+        content={mockContentArrayVehicles}
+        theme="vehicle"
+        addToFavorites={mockAddToFavorites}
+        removeFromFavorites={mockRemoveFromFavorites}
+        logError={mockLogError}
+      />
     );
   });
-  it("should render without errors", () => {
-    expect(toJSON(wrapper)).toMatchSnapshot();
+
+  it("should match the snapshot per each theme", () => {
+    expect(toJSON(personTheme)).toMatchSnapshot();
+    expect(toJSON(planetTheme)).toMatchSnapshot();
+    expect(toJSON(vehicleTheme)).toMatchSnapshot();
   });
 });
