c64aaa8ec238f136964f0c73818b8697311ef66b
Kevin Krato <kratok08@gmail.com>
2019-07-09 17:16:09 -0600

    Extract further mockData to mockData directory

--- src/components/ContentSection/ContentSection.test.js
+++ src/components/ContentSection/ContentSection.test.js
@@ -2,83 +2,12 @@ import React from "react";
 import { shallow } from "enzyme";
 import toJSON from "enzyme-to-json";
 import ContentSection from "./ContentSection";
+import {
+  mockContentArrayPeople,
+  mockContentArrayPlanets,
+  mockContentArrayVehicles
+} from "../../utils/mockData";
 
-const mockContentArrayPeople = [
-  {
-    name: "Joe",
-    gender: "Male",
-    hair_color: "Green",
-    eye_color: "Blue",
-    birth_year: 1995,
-    mass: 120,
-    height: 72,
-    id: 1
-  },
-  {
-    name: "Tony",
-    gender: "Unknown",
-    hair_color: "Orange",
-    eye_color: "Yellow",
-    birth_year: 1092,
-    mass: 10,
-    height: 150,
-    id: 2
-  }
-];
-
-const mockContentArrayPlanets = [
-  {
-    name: "Naboo",
-    climate: "Moderate",
-    terrain: "Grasslands",
-    population: 5000000,
-    rotation_period: 24,
-    orbital_period: 375,
-    diameter: 15,
-    favorited: false,
-    id: 1
-  },
-  {
-    name: "Tatooine",
-    climate: "Dry",
-    terrain: "Desert",
-    population: 50000,
-    rotation_period: 34,
-    orbital_period: 475,
-    diameter: 9,
-    favorited: false,
-    id: 2
-  }
-];
-
-const mockContentArrayVehicles = [
-  {
-    name: "Speeder",
-    model: "Super",
-    vehicle_class: "X12",
-    manufacturer: "Toyota",
-    const_in_credits: 15000,
-    max_atmosphere_speed: 50,
-    crew: 15,
-    passengers: 30,
-    cargo_capacity: 5,
-    favorited: false,
-    id: 1
-  },
-  {
-    name: "Slowboy",
-    model: "Sloweest",
-    vehicle_class: "z12",
-    manufacturer: "Yamaha",
-    const_in_credits: 150000,
-    max_atmosphere_speed: 1,
-    crew: 1,
-    passengers: 1,
-    cargo_capacity: 0,
-    favorited: false,
-    id: 2
-  }
-];
 describe("<ContentSection />", () => {
   let personTheme, planetTheme, vehicleTheme;
   const mockAddToFavorites = jest.fn();
